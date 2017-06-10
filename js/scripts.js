b2ae0f1ab1a1ba9f229ca182d7e52735c0ee8984
Iroveena Pagsibigan <iraexcel13@gmail.com>
2017-06-09 22:02:15 -0700

    Add css styles and js scripts

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $("#button").click(function() {
+
+    var first = $("#first1").val();
+    var age = $("#age1").val();
+    var kids = $("#kids1").val();
+    var beach = $("#beach1").val();
+    var activity = $("activity1").val();
+    var days = $("day1").val();
+
+
+    if (first === "Yes" || "No" && age === "below 21" && kids === "Yes" && beach === "No" && activity === "Theme Park" &&
+    days === "3 days") {
+      $("#disneyland").show();
+    }
+    else if (first === "Yes" || "No" && age === "21 and over" && kids === "No" && beach === "No" && activity === "Entertainment" &&
+    days === "7 days or more") {
+      $("#lasvegas").show();
+    }
+
+    else if (first === "No" && age === "21 and over" && kids === "Yes" && beach === "Yes" && activity === "Sightseeing" &&
+    days === "5 days") {
+      $("#hawaii").show();
+    }
+
+    else {
+      $("#house").show();
+    }
+
+    
+  });
+
+});
