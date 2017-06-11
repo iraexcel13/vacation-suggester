59461c8941b218a9430cc8a25e7ad1ecf440343f
Iroveena Pagsibigan <iraexcel13@gmail.com>
2017-06-10 21:05:20 -0700

    Add more conditions on if else statements.

--- js/scripts.js
+++ js/scripts.js
@@ -10,17 +10,16 @@ $(document).ready(function() {
     var days = $("day1").val();
 
 
-    if (first === "Yes" || "No" && age === "below 21" && kids === "Yes" && beach === "No" && activity === "Theme Park" &&
-    days === "3 days") {
+    if (first === "Yes" || "No" && age === "below 21" || "21 and over" && kids === "Yes" && beach === "Yes" || "No" && activity === "Theme Park" &&
+    days === "3 days" || "5 days" || "7 days or more") {
       $("#disneyland").show();
     }
     else if (first === "Yes" || "No" && age === "21 and over" && kids === "No" && beach === "No" && activity === "Entertainment" &&
-    days === "7 days or more") {
+    days === "3 days" || "5 days" || "7 days or more") {
       $("#lasvegas").show();
     }
-
-    else if (first === "Yes" || "No" && age === "21 and over" && kids === "Yes" && beach === "Yes" && activity === "Sightseeing" &&
-    days === "5 days") {
+    else if (first === "Yes" || "No" && age === "21 and over" || "below 21" && kids === "Yes" || "No" && beach === "Yes" && activity === "Sightseeing" &&
+    days === "3 days" || "5 days" || "7days or more") {
       $("#hawaii").show();
     }
 
